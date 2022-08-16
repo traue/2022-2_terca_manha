37317a2d179e9bc81af26b9c727fa5dc8e32f248
Prof. Thiago G. Traue <t.traue@gmail.com>
2022-08-16 10:31:49 -0300

    Primeiras aulas de C

---
+++ 0 - Mini curso de C/0-HelloWorld/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+    //isso é um comentário!
+
+    /*
+        Isso é um bloco de
+        comentário! Muito legal, não?!
+    */
+
+    printf("Olá pessoal, tudo bem?\nEspero que estejam gostando!");
+
+    printf("\nO professor \"Traue\" é legal!");
+    
+    return 0; 
+}
