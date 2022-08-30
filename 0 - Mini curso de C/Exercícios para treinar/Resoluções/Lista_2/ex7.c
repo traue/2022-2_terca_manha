87ad3303f33c2601f52905aef686f3dd873d078d
Prof. Thiago G. Traue <t.traue@gmail.com>
2022-08-30 09:41:38 -0300

    Continuação

---
+++ 0 - Mini curso de C/Exercícios para treinar/Resoluções/Lista_2/ex7.c
@@ -0,0 +1,15 @@
+/*
+    7. Construa um programa que leia o percurso em 
+    quilómetros, o tipo do carro e informe o consumo 
+    estimado de combustível, sabendo-se que um carro 
+    tipo A faz 12 km com um litro de gasolina, um tipo 
+    B faz 9km e o tipo C, 8km por litro.
+*/
+
+
+#include <stdio.h>
+
+int main() {
+    
+    return 0;
+}
