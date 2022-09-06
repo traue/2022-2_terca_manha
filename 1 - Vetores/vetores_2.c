5c423af465d516414c87a99c54b9c0b61d1ff875
Prof. Thiago G. Traue <t.traue@gmail.com>
2022-09-06 09:31:25 -0300

    Continuação

--- 1 - Vetores/vetores_2.c
+++ 1 - Vetores/vetores_2.c
@@ -3,4 +3,25 @@
     NUM e imprima uma listagem dos números lidos. Utilize um laço "for"
 */
 
-//faremos semana que vem
+#include <stdio.h>
+
+int main() {
+
+    //variáveis necessárias
+    int NUM[15];
+
+    //entrada
+    for(int i = 0; i < 15; i++) {
+        printf("Informe o %dº valor: ", (i + 1));
+        scanf("%d", &NUM[i]);
+    }
+
+    printf("\n-----veja os dados informados-----\n");
+
+    //impressão:
+    for(int i = 0; i < 15; i++) {
+        printf("\nValor na %dª posição: %d", (i + 1), NUM[i]);
+    }
+
+    return 0;
+}
--- 1 - Vetores/vetores_3.c
+++ 1 - Vetores/vetores_3.c
@@ -1,9 +1,28 @@
 /*
-    PARA TREINAR A MENTE...:
-
     Faça um programa em C que leia 5 valores inteiros e 
     armazene em um vetor. Após a leitura o programa deve
     encontrar e imprimir o maior valor armazenado no vetor.
 */
 
-//faremos semana que vem
+#include <stdio.h>
+
+int main() {
+
+    //variáveis necessárias
+    int valores[5], maior;
+
+    //entrada
+    for(int i = 0; i < 5; i++) {
+        printf("Informe o %dº valor: ", (i + 1));
+        scanf("%d", &valores[i]);
+    }
+
+    maior = valores[0];
+
+    for(int i = 1; i < 5; i++) {
+        if(maior < valores[i])
+            maior = valores[i];
+    }
+
+    printf("\nMaior valor digitado: %d", maior);
+}
